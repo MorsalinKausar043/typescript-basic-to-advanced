c2ef08c6c0132dfebd720a5651d26c71e5aa4f9e
Morsalin <morsalinkausar043@gmail.com>
2023-05-05 16:04:24 +0600

    add discount system in getter and setter

--- dist/getters-setters.js
+++ dist/getters-setters.js
@@ -1,30 +1,49 @@
 "use strict";
-class bankAccount {
-    constructor(name, age, _balance) {
+// class bankAccount {
+//     public readonly name: string;
+//     protected age: number;
+//     private _balance: number;
+//     constructor(name: string, age: number, _balance: number) {
+//         this.name = name;
+//         this.age = age;
+//         this._balance = _balance;
+//     };
+//     private getBalance(): number {
+//         return this._balance;
+//     };
+//     test(): number {
+//         return this.getBalance();
+//     }
+//     // getters  
+//     get getDeposit(): number {
+//         return this._balance;
+//     };
+//     // setters  
+//     set setDeposit(num: number) {
+//         this._balance = this._balance * num;
+//     }
+// };
+// const myAccount = new bankAccount("muhammad", 63, 500);
+// console.log(myAccount);
+// myAccount.setDeposit = 20;
+// console.log(myAccount);
+class shop {
+    constructor(id, bill, name) {
+        this.id = id;
+        this.bill = bill;
         this.name = name;
-        this.age = age;
-        this._balance = _balance;
     }
-    ;
-    getBalance() {
-        return this._balance;
+    // getters 
+    get getBill() {
+        return this.bill;
     }
-    ;
-    test() {
-        return this.getBalance();
-    }
-    // getters  
-    get getDeposit() {
-        return this._balance;
-    }
-    ;
     // setters  
-    set setDeposit(num) {
-        this._balance = this._balance * num;
+    set discountBill(disVal) {
+        this.bill = this.bill - (disVal / 100) * this.bill;
     }
 }
 ;
-const myAccount = new bankAccount("muhammad", 63, 500);
-console.log(myAccount);
-myAccount.setDeposit = 20;
-console.log(myAccount);
+const billOne = new shop(2023, 1000, "dhaka");
+console.log(billOne.getBill);
+billOne.discountBill = 10;
+console.log(billOne.getBill);
--- src/getters-setters.ts
+++ src/getters-setters.ts
@@ -1,33 +1,60 @@
-class bankAccount {
-    public readonly name: string;
-    protected age: number;
-    private _balance: number;
-    constructor(name: string, age: number, _balance: number) {
+// class bankAccount {
+//     public readonly name: string;
+//     protected age: number;
+//     private _balance: number;
+//     constructor(name: string, age: number, _balance: number) {
+//         this.name = name;
+//         this.age = age;
+//         this._balance = _balance;
+//     };
+
+//     private getBalance(): number {
+//         return this._balance;
+//     };
+
+//     test(): number {
+//         return this.getBalance();
+//     }
+//     // getters  
+//     get getDeposit(): number {
+//         return this._balance;
+//     };
+
+//     // setters  
+//     set setDeposit(num: number) {
+//         this._balance = this._balance * num;
+//     }
+// };
+
+// const myAccount = new bankAccount("muhammad", 63, 500);
+
+// console.log(myAccount);
+// myAccount.setDeposit = 20;
+// console.log(myAccount);
+
+// type class and discount product bill  in typescript
+class shop{
+    private id:number;
+    protected bill:number;
+    protected name: string;
+    constructor(id:number,bill:number,name:string) {
+        this.id = id;
+        this.bill = bill;
         this.name = name;
-        this.age = age;
-        this._balance = _balance;
-    };
-
-    private getBalance(): number {
-        return this._balance;
-    };
-
-    test(): number {
-        return this.getBalance();
     }
-    // getters  
-    get getDeposit(): number {
-        return this._balance;
-    };
 
+    // getters 
+    get getBill():number{
+        return this.bill;
+    }
     // setters  
-    set setDeposit(num: number) {
-        this._balance = this._balance * num;
+    set discountBill(disVal:number){
+        this.bill = this.bill - (disVal/100) * this.bill;
     }
 };
 
-const myAccount = new bankAccount("muhammad", 63, 500);
+const billOne = new shop(2023,1000,"dhaka");
 
-console.log(myAccount);
-myAccount.setDeposit = 20;
-console.log(myAccount);
+console.log(billOne.getBill);
+billOne.discountBill = 10;
+console.log(billOne.getBill);
