32be25724b4bb3427c2e031f4777ce639b6e092f
Morsalin <morsalinkausar043@gmail.com>
2023-05-07 14:32:39 +0600

    done practice

--- dist/practice-day.js
+++ dist/practice-day.js
@@ -17,7 +17,7 @@
 // compareArrays(array1, array2)
 // // console.log(compareArrays(array1, array2))
 // 3. You have an interface for Product, containing the product's id, name, price, and category. You want to filter an array of Products based on a specific criterion and value. 
-// // define productType 
+// define productType 
 // interface productType {
 //     id: number,
 //     name: string,
@@ -26,19 +26,25 @@
 // };
 // // Define the Products array
 // const products: productType[] = [
-//     { id: 1, name: 'Product A', price: 10, category: 'Category A' },
-//     { id: 2, name: 'Product B', price: 20, category: 'Category B' },
-//     { id: 3, name: 'Product C', price: 30, category: 'Category A' },
-//     { id: 4, name: 'Product D', price: 40, category: 'Category B' },
-//     { id: 5, name: 'Product E', price: 50, category: 'Category A' }
+//     { id: 1, name: 'chips', price: 10, category: 'food' },
+//     { id: 2, name: 'apple', price: 20, category: 'fruit' },
+//     { id: 3, name: 't-shirt', price: 30, category: 'cloth' },
+//     { id: 4, name: 'pine-apple', price: 40, category: 'fruit' },
+//     { id: 5, name: 'mango', price: 50, category: 'fruit' }
 // ];
 // // Define the criterion and value to filter by
 // const criterion = 'category';
 // const value = 'Category B';
 // // Use Array.filter() to filter the Products array
-// const filteredProducts = products.filter(product => product[criterion] === value);
-// // const filteredProducts = products.filter(product => product.category === value);
-// // console.log(filteredProducts);
+// const searchProduct = <T extends productType>(productArr: T[], criteria: keyof T, value: string | number): T[] | string => {
+//     const search = productArr.filter(product => product[criteria] === value);
+//     if(search.length <= 0){
+//         return `Api Data Not find!`
+//     }
+//     return search;
+// }
+// const searchAPI = searchProduct(products, "category", "fruit")
+// console.log(searchAPI);
 // 4. Suppose you have an array of tuples, where each tuple represents a product and contains the product name, price, and quantity. Write a TypeScript function that calculates the total cost of all the products in the array, using a generic type for the tuple and a type alias for the array.
 // type productsType = [string, number, number];
 // const calculateTotalProduct = (product: productsType[]): number => {
@@ -50,7 +56,7 @@
 // };
 // const oneProducts = calculateTotalProduct([["dove", 530, 7], ["dove", 780, 2]]);
 // console.log(oneProducts);
-// another solve 
+// another solve
 // type ProductsType = [string, number,number];
 // type productArray = ProductsType[];
 // const calculateTotalCost = <T extends productArray>(products : T):number =>{
@@ -69,15 +75,63 @@
 // const totalResult = calculateTotalCost(manyProducts);
 // console.log(totalResult);
 // 5. Suppose you have an array of numbers in TypeScript, and you want to find the sum of all the even numbers in the array.How would you approach this problem and write code to solve it ?
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-const sumEvenNum = (numbers) => {
-    let sumVal = 0;
-    for (let i = 0; i < numbers.length; i++) {
-        const num = numbers[i];
-        if ((num % 2) === 0) {
-            sumVal += num;
-        }
+// const numbers:number[] = [1,2,3,4,5,6,7,8,9];
+// const sumEvenNum = (numbers:number[]):number =>{
+//     let sumVal = 0;
+//     for(let i = 0; i < numbers.length;i++){
+//         const num = numbers[i];
+//         if((num % 2) === 0){
+//             sumVal += num;
+//         }
+//     }
+//     return sumVal;
+// };
+// console.log(sumEvenNum(numbers));
+// 6. Create an interface called Person that includes properties for name(string), age(number), and email(string).Then create an array of Person objects and write a function that takes the array and a string email as parameters, and returns the Person object that matches the email or null if no match is found.
+// interface Person{
+//     name:string;
+//     age:number;
+//     email:string;
+// };
+// const personApi:Person[] = [
+//     {name:"morsalin", age:20, email:"morsalinkausar043@gmail.com"},
+//     {name:"jakir", age:20, email:"jihadislam247@gmail.com"},
+//     {name:"pranto", age:20, email:"pranto420@gmail.com"},
+// ];
+// const searchUserApi = <T extends Person>(personApi:T[], value:string) : T[] | string =>{
+//     const search = personApi.filter(person => person.email === value);
+//     if(search.length <= 0){
+//         return "Person Not Found!"
+//     }
+//     return search;
+// };
+// const getPerson = searchUserApi(personApi, "morsalinkausar043@gmail.com");
+// console.log(getPerson)
+// 7. Create a TypeScript program that declares an array of numbers.Use the spread  operator to pass the elements of the array as arguments to a function that finds the minimum and maximum values of the array.Use destructuring to assign the minimum and maximum values to separate variables, and log them to the console.
+// const array1:number[] = [2,4,8,20,16];
+// const maxMin = (...numbers:number[]): [number, number] =>{
+//     let maxNum = numbers[0];
+//     let minNum = numbers[0];
+//     for(let i = 0; i < numbers.length; i++){
+//         if(Math.max(numbers[i])){
+//             maxNum = numbers[i]
+//         }
+//         else if(Math.min(numbers[i])){
+//             minNum = numbers[i]
+//         }
+//     }
+//     return [maxNum, minNum];
+// };
+// console.log(maxMin(...array1));
+// 8. Create a TypeScript program that declares a function that takes a string parameter with a literal type of "red", "green", or "blue", and an optional boolean parameter. If the boolean parameter is true, log the string parameter in uppercase. If the boolean parameter is false or not provided, log the string parameter in lowercase.
+const color = (color, toUpperCase) => {
+    if (toUpperCase) {
+        return color.toLocaleUpperCase();
     }
-    return sumVal;
+    else {
+        return color.toLocaleLowerCase();
+    }
 };
-console.log(sumEvenNum(numbers));
+console.log(color("red")); // red
+console.log(color("red", true)); // RED
+console.log(color("blue", true)); // BLUE
--- src/practice-day.ts
+++ src/practice-day.ts
@@ -42,7 +42,7 @@
 
 
 
-// // define productType 
+// define productType 
 // interface productType {
 //     id: number,
 //     name: string,
@@ -52,11 +52,11 @@
 
 // // Define the Products array
 // const products: productType[] = [
-//     { id: 1, name: 'Product A', price: 10, category: 'Category A' },
-//     { id: 2, name: 'Product B', price: 20, category: 'Category B' },
-//     { id: 3, name: 'Product C', price: 30, category: 'Category A' },
-//     { id: 4, name: 'Product D', price: 40, category: 'Category B' },
-//     { id: 5, name: 'Product E', price: 50, category: 'Category A' }
+//     { id: 1, name: 'chips', price: 10, category: 'food' },
+//     { id: 2, name: 'apple', price: 20, category: 'fruit' },
+//     { id: 3, name: 't-shirt', price: 30, category: 'cloth' },
+//     { id: 4, name: 'pine-apple', price: 40, category: 'fruit' },
+//     { id: 5, name: 'mango', price: 50, category: 'fruit' }
 // ];
 
 // // Define the criterion and value to filter by
@@ -64,10 +64,16 @@
 // const value = 'Category B';
 
 // // Use Array.filter() to filter the Products array
-// const filteredProducts = products.filter(product => product[criterion] === value);
-// // const filteredProducts = products.filter(product => product.category === value);
+// const searchProduct = <T extends productType>(productArr: T[], criteria: keyof T, value: string | number): T[] | string => {
 
-// // console.log(filteredProducts);
+//     const search = productArr.filter(product => product[criteria] === value);
+//     if(search.length <= 0){
+//         return `Api Data Not find!`
+//     }
+//     return search;
+// }
+// const searchAPI = searchProduct(products, "category", "fruit")
+// console.log(searchAPI);
 
 
 
@@ -94,7 +100,7 @@
 
 
 
-// another solve 
+// another solve
 
 // type ProductsType = [string, number,number];
 // type productArray = ProductsType[];
@@ -122,26 +128,82 @@
 
 // 5. Suppose you have an array of numbers in TypeScript, and you want to find the sum of all the even numbers in the array.How would you approach this problem and write code to solve it ?
 
-const numbers:number[] = [1,2,3,4,5,6,7,8,9];
+// const numbers:number[] = [1,2,3,4,5,6,7,8,9];
 
-const sumEvenNum = (numbers:number[]):number =>{
-    let sumVal = 0;
-    for(let i = 0; i < numbers.length;i++){
-        const num = numbers[i];
+// const sumEvenNum = (numbers:number[]):number =>{
+//     let sumVal = 0;
+//     for(let i = 0; i < numbers.length;i++){
+//         const num = numbers[i];
 
-        if((num % 2) === 0){
-            sumVal += num;
-        }
-    }
+//         if((num % 2) === 0){
+//             sumVal += num;
+//         }
+//     }
 
-    return sumVal;
-};
+//     return sumVal;
+// };
 
-console.log(sumEvenNum(numbers));
+// console.log(sumEvenNum(numbers));
 
 
 // 6. Create an interface called Person that includes properties for name(string), age(number), and email(string).Then create an array of Person objects and write a function that takes the array and a string email as parameters, and returns the Person object that matches the email or null if no match is found.
 
+// interface Person{
+//     name:string;
+//     age:number;
+//     email:string;
+// };
+
+// const personApi:Person[] = [
+//     {name:"morsalin", age:20, email:"morsalinkausar043@gmail.com"},
+//     {name:"jakir", age:20, email:"jihadislam247@gmail.com"},
+//     {name:"pranto", age:20, email:"pranto420@gmail.com"},
+// ];
+
+// const searchUserApi = <T extends Person>(personApi:T[], value:string) : T[] | string =>{
+//     const search = personApi.filter(person => person.email === value);
+//     if(search.length <= 0){
+//         return "Person Not Found!"
+//     }
+//     return search;
+// };
+
+// const getPerson = searchUserApi(personApi, "morsalinkausar043@gmail.com");
+// console.log(getPerson)
+
 // 7. Create a TypeScript program that declares an array of numbers.Use the spread  operator to pass the elements of the array as arguments to a function that finds the minimum and maximum values of the array.Use destructuring to assign the minimum and maximum values to separate variables, and log them to the console.
 
+// const array1:number[] = [2,4,8,20,16];
+
+// const maxMin = (...numbers:number[]): [number, number] =>{
+//     let maxNum = numbers[0];
+//     let minNum = numbers[0];
+//     for(let i = 0; i < numbers.length; i++){
+//         if(Math.max(numbers[i])){
+//             maxNum = numbers[i]
+//         }
+//         else if(Math.min(numbers[i])){
+//             minNum = numbers[i]
+//         }
+//     }
+//     return [maxNum, minNum];
+// };
+
+
+// console.log(maxMin(...array1));
+
+
 // 8. Create a TypeScript program that declares a function that takes a string parameter with a literal type of "red", "green", or "blue", and an optional boolean parameter. If the boolean parameter is true, log the string parameter in uppercase. If the boolean parameter is false or not provided, log the string parameter in lowercase.
+
+const color = (color: "red" | "green" | "blue", toUpperCase?:boolean) :string =>{
+    if(toUpperCase){
+        return color.toLocaleUpperCase();
+    }else{
+        return color.toLocaleLowerCase();
+    }
+};
+
+console.log(color("red",)) // red
+console.log(color("red",true)) // RED
+console.log(color("blue",true)) // BLUE
+
